7cc409c76ecdb34160c8ef06e5b7bce7c60d42bc
HSN <hasanlock@gmail.com>
2022-08-16 22:15:08 +0600

    refactor: remove leading spaces

--- src/examples/http-post-with-auth-bearer.ts
+++ src/examples/http-post-with-auth-bearer.ts
@@ -4,18 +4,18 @@ export default <ExampleItem>{
   label: 'http: POST with header and bearer token',
   description: '',
   snippet: `
-  ---
-  version: default:http:0.7.2
-  
-  request:
-    url: https://httpbin.org/post
-    method: POST
-  
-    headers:
-      Accept-Encoding: gzip, deflate
-      Content-Type: application/json
-  
-    auth[bearer]:
-      token: eyJhbGciOiJIU4fwpMeJf36POk6yJV_adQssw5c  
+---
+version: default:http:0.7.2
+
+request:
+  url: https://httpbin.org/post
+  method: POST
+
+  headers:
+    Accept-Encoding: gzip, deflate
+    Content-Type: application/json
+
+  auth[bearer]:
+    token: eyJhbGciOiJIU4fwpMeJf36POk6yJV_adQssw5c  
 `,
 };
