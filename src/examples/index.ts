221749b92c25ac05726cb2ee6b05f6f2a3f66275
HSN <hasanlock@gmail.com>
2022-08-16 22:27:36 +0600

    feat: integrate all example

--- src/examples/index.ts
+++ src/examples/index.ts
@@ -1,6 +1,9 @@
 import minimalRequest from "./minimal-request";
 import httpGETWithURLParams from "./http-get-with-query-string";
 import httpPOSTWithAuthBearer from "./http-post-with-auth-bearer";
+import httpPOSTWithFormSubmit from "./http-post-form-submit";
+import httpPOSTWithJSONBody from "./http-post-with-json-body";
+import httpPUTWithFileUpload from "./http-put-with-file-upload";
 
 export interface ExampleItem {
   label: string;
@@ -12,4 +15,7 @@ export default [
   minimalRequest, 
   httpGETWithURLParams,
   httpPOSTWithAuthBearer,
+  httpPOSTWithFormSubmit,
+  httpPOSTWithJSONBody,
+  httpPUTWithFileUpload,
 ];
