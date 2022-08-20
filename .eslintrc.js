6cc9266bf75b71dcbb49378f16381716ce734620
Ariful Alam <swazan.arif@gmail.com>
2022-08-20 16:47:05 +0600

    chore: create .eslintrc.js

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+// .eslintrc.js
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+    mocha: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+};
