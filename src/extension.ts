34e61b0cc8985fcb720270ff02fc222d6cd465be
HSN <hasanlock@gmail.com>
2023-02-04 14:30:09 +0600

    refactor: general refactor

--- src/extension.ts
+++ src/extension.ts
@@ -2,16 +2,9 @@ import * as vscode from "vscode";
 import { addHttpSnippet } from "./add-http-snippet";
 
 export function activate(context: vscode.ExtensionContext) {
-  const disposable = vscode.commands.registerCommand(
-    "chkware.addHttpSnippet",
-    () => {
-      addHttpSnippet();
-    }
+  context.subscriptions.push(
+    vscode.commands.registerCommand("chkware.addHttpSnippet", () =>
+      addHttpSnippet()
+    )
   );
-
-  context.subscriptions.push(disposable);
-}
-
-export function deactivate() {
-  //
 }
