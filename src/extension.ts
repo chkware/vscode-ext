105505a9d7caca9fad98e82aebfedf215c5871d0
HSN <hasanlock@gmail.com>
2023-02-04 14:33:42 +0600

    refactor: rename snippet loading service

--- src/extension.ts
+++ src/extension.ts
@@ -1,5 +1,5 @@
 import * as vscode from "vscode";
-import { addHttpSnippet } from "./add-http-snippet";
+import { addHttpSnippet } from "./add-snippet";
 
 export function activate(context: vscode.ExtensionContext) {
   context.subscriptions.push(
