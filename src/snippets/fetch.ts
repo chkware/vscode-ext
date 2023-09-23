26f4020c02900ef79b47f21f59cb6e3ee3970001
HSN <hasanlock@gmail.com>
2023-09-23 19:55:17 +0600

    feat: add fetch snippets

---
+++ src/snippets/fetch.ts
@@ -0,0 +1,120 @@
+import { SnippetItem } from ".";
+
+export const httpGETWithURLParams = <SnippetItem>{
+  label: "http: GET with query string",
+  description: "",
+  snippet: `---
+version: default:http:0.7.2
+
+request:
+  url: https://httpbin.org/get
+
+  url_params:
+    foo: bar
+    two: 2
+  method: GET
+`,
+};
+
+export const httpPOSTWithAuthBearer = <SnippetItem>{
+  label: "http: POST form submit",
+  description: "",
+  snippet: `---
+version: default:http:0.7.2
+
+request:
+  url: https://httpbin.org/post
+  method: POST
+
+  headers:
+    Accept-Encoding: gzip, deflate
+
+  auth .scm=bearer:
+    token: eyJhbGciOiJIU4fwpMeJf36POk6yJV_adQssw5c
+
+  body .enc=form:
+    user_id: 32,
+    roll_no: 1,
+    class: 2,
+    name: 'Student name'
+
+    # note: this will just set photo with file path as string, 
+    # to upload the actual file use "body .enc=form-data" instead
+    photo: file:///home/username/student-photo-01.png
+`,
+};
+
+export const httpPOSTWithFormSubmit = <SnippetItem>{
+  label: "http: POST with header and bearer token",
+  description: "",
+  snippet: `---
+version: default:http:0.7.2
+
+request:
+  url: https://httpbin.org/post
+  method: POST
+
+  headers:
+    Accept-Encoding: gzip, deflate
+    Content-Type: application/json
+
+  auth .scm=bearer:
+    token: eyJhbGciOiJIU4fwpMeJf36POk6yJV_adQssw5c  
+`,
+};
+
+export const httpPOSTWithJSONBody = <SnippetItem>{
+  label: "http: POST with JSON body",
+  description: "",
+  snippet: `---
+version: default:http:0.7.2
+
+request:
+  url: https://httpbin.org/post
+  method: POST
+
+  headers:
+    Accept-Encoding: gzip, deflate
+    Content-Type: application/json
+
+  auth .scm=bearer:
+    token: eyJhbGciOiJIU4fwpMeJf36POk6yJV_adQssw5c  
+`,
+};
+
+export const httpPUTWithFileUpload = <SnippetItem>{
+  label: "http: PUT with file upload",
+  description: "",
+  snippet: `---
+version: default:http:0.7.2
+
+request:
+  url: https://httpbin.org/put
+  method: PUT
+
+  headers:
+    Accept-Encoding: gzip, deflate
+
+  auth .scm=bearer:
+    token: eyJhbGciOiJIU4fwpMeJf36POk6yJV_adQssw5c
+
+  body .enc=form-data:
+    user_id: 32,
+    roll_no: 1,
+
+    # note: this will upload the file
+    cover_photo: file:///home/username/student-cvphoto-01.png
+`,
+};
+
+export const minimalRequest = <SnippetItem>{
+  label: "http: Minimal request",
+  description: "",
+  snippet: `---
+version: default:http:0.7.2
+
+request:
+  url: https://httpbin.org/get
+  method: GET
+`,
+};
